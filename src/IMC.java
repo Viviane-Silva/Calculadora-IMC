bdf96cad371f3c8e724ed7b1f9662b7030654148
Viviane-Silva <viviane.leite.silva@gmail.com>
2021-04-08 19:53:32 -0300

    teste inicial

---
+++ src/IMC.java
@@ -0,0 +1,5 @@
+public class IMC {
+    public static void main(String args[]) {
+        double altura, peso, imc;
+    }
+}
---
+++ src/OlaMundo.java
@@ -0,0 +1,16 @@
+
+import javax.swing.JOptionPane;
+
+public class OlaMundo {
+    public static void main (String args[])
+    {
+
+        // int = numeros inteiro (1,20,123,3)
+        // = o igual se lê  recebe*
+        // double = numeros fracionados (1,30)
+        // string = palavras + aspas ("Viviane","Casa")
+        // boolean = verdadeiro ou falso (true , false)
+        //System.out.println("Olá Mundo, aqui está tudo bem :D");
+     JOptionPane.showMessageDialog(null,  "Olá Mundo, aqui está tudo bem :D");
+        }
+    }

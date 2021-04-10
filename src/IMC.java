3e3a6c86875c675510c3bbc54f0f9b965fcd9771
Viviane-Silva <viviane.leite.silva@gmail.com>
2021-04-10 20:04:01 -0300

    finilized test

--- src/IMC.java
+++
@@ -1,5 +0,0 @@
-public class IMC {
-    public static void main(String args[]) {
-        double altura, peso, imc;
-    }
-}
---
+++ src/calculadoraIMC.java
@@ -0,0 +1,18 @@
+import javax.swing.JOptionPane;
+import java.text.DecimalFormat;
+
+public class calculadoraIMC {
+    public static void main(String args[]) {
+        double altura, peso, imc;
+
+        altura = Double.parseDouble(JOptionPane.showInputDialog("Digite sua altura:"));
+        peso = Double.parseDouble(JOptionPane.showInputDialog("Digite seu peso:"));
+
+        imc = peso / (altura * altura);
+
+        DecimalFormat decimal = new DecimalFormat("0.00");
+        String valorFormatado = decimal.format(imc);
+
+        JOptionPane.showMessageDialog(null, "IMC = " + valorFormatado);
+    }
+}
